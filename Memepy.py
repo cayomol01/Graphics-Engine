b9cf0d22a8445c429106c9f1308430d976d5b903
TheJPMZ <monzon.jpmz@gmail.com>
2022-08-08 20:55:22 -0600

    ♻ Renamed Numeme to Memepy.py

--- Memepy.py
+++ Memepy.py
@@ -1,9 +1,8 @@
-
 class Matrix(list):
     def __matmul__(self, B):
         # Fuente: https://www.codingem.com/numpy-at-operator/
         # Equivalente a @
-        return [sum(a*b for a,b in zip(C,B)) for C in A]  
+        return [sum(a*b for a,b in zip(C,B)) for C in self]  
 
 def mul(A, B):
     # Equivalente a *
