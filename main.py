1dbf64f17c58b6ac251370edce7201e8a9876ad3
TheJPMZ <monzon.jpmz@gmail.com>
2022-08-20 00:08:18 -0600

    ➕ Negative shader

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ import random
 height = 960
 width = 540
 
-window = Window(height, width,("red"),("white"))
+window = Window(height, width,("black"),("white"))
 
-window.active_shader = window.shaders["duality"]
+window.active_shader = window.shaders["flat"]
 window.active_texture = Texture("ash.bmp")
 
-window.loadObject("carro.obj",translate=(width-50, height/6, 0), rotate=(0,180,0),scale=(20, 20, 20)) # Medium Shot
+window.loadObject("carro.obj",translate=(width-50, height/6, 0), rotate=(-10,190,0),scale=(20, 20, 20)) # Medium Shot
 window.finish("Meme")
 
 #window.clear()
--- shaders.py
+++ shaders.py
@@ -97,3 +97,32 @@ def duality(render, **kwargs):
         return (0, 0, 0)
     
     return (1 - r * intensity, 1- g * intensity, 1 - b * intensity)
+def negative(render, **kwargs):
+    
+    u, v, w = kwargs["bary_coords"]
+    b, g, r = kwargs["v_color"]
+    tA, tB, tC = kwargs["text_coords"]
+    triangleNormal = kwargs["triangle_normal"]
+    
+    r /= 255
+    g /= 255
+    b /= 255
+    
+    if render.active_texture:
+        
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+        
+        texture_color = render.active_texture.getColor(tU, tV)
+        
+        r *= texture_color[0]
+        g *= texture_color[1]
+        b *= texture_color[2]
+        
+    intensity = dot(triangleNormal, render.light_direction) * -1
+    
+    if intensity < 0:
+        return (0, 0, 0)
+    
+    return (1 - r * intensity, 1 - g * intensity, 1 - b * intensity)
+
