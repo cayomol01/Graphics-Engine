4b8e1d53b1459f47bb25f5e35cf34645903b7d1a
TheJPMZ <monzon.jpmz@gmail.com>
2022-08-20 00:09:04 -0600

    ➕Pride Shader

--- shaders.py
+++ shaders.py
@@ -165,3 +165,55 @@ def dissolvefade(render, **kwargs):
     
     return (r * intensity, g * intensity, b * intensity)
 
+
+def pride(render, **kwargs):
+    
+    u, v, w = kwargs["bary_coords"]
+    b, g, r = kwargs["v_color"]
+    tA, tB, tC = kwargs["text_coords"]
+    nA, nB, nC = kwargs["normals"]
+    triangleNormal = kwargs["triangle_normal"]
+       
+    r /= 255
+    g /= 255
+    b /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = [nA[0] * u + nB[0] * v + nC[0] * w,
+                      nA[1] * u + nB[1] * v + nC[1] * w,
+                      nA[2] * u + nB[2] * v + nC[2] * w]   
+
+    intensity = dot(triangleNormal, render.light_direction) * -1
+    
+    if intensity < 0:
+        return (0 * 0.9, 0 * 0.9, 0 * 0.9)
+    elif intensity < 0.1:
+        return (0 * 0.9, 0 * 0.9, 1 * 0.9)
+    elif intensity < 0.2:
+        return (0 * 0.9, 0.5 * 0.9, 1 * 0.9)
+    elif intensity < 0.3:
+        return (0 * 0.9, 1 * 0.9, 1 * 0.9)
+    elif intensity < 0.4:
+        return (0 * 0.9, 1 * 0.9, 0.5 * 0.9)
+    elif intensity < 0.5:
+        return (0.5 * 0.9, 1 * 0.9, 0 * 0.9)
+    elif intensity < 0.6:
+        return (1 * 0.9, 1 * 0.9, 0 * 0.9)
+    elif intensity < 0.7:
+        return (1 * 0.9, 0.5 * 0.9, 0 * 0.9)
+    elif intensity < 0.8:
+        return (1 * 0.9, 0 * 0.9, 0 * 0.9)
+    elif intensity < 0.9:
+        return (1 * 0.9, 0 * 0.9, 0.5 * 0.9)
+    elif intensity < 1:
+        return (1 * 0.9, 0 * 0.9, 1 * 0.9)
