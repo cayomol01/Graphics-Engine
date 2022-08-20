9fd150b9d570ba76cbe6f89740c1d9e40cb73e03
TheJPMZ <monzon.jpmz@gmail.com>
2022-08-20 00:08:48 -0600

    ➕dissolvefade shader

--- shaders.py
+++ shaders.py
@@ -126,3 +126,42 @@ def negative(render, **kwargs):
     
     return (1 - r * intensity, 1 - g * intensity, 1 - b * intensity)
 
+def dissolvefade(render, **kwargs):
+    
+    u, v, w = kwargs["bary_coords"]
+    b, g, r = kwargs["v_color"]
+    tA, tB, tC = kwargs["text_coords"]
+    nA, nB, nC = kwargs["normals"]
+    triangleNormal = kwargs["triangle_normal"]
+       
+    r /= 255
+    g /= 255
+    b /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = [nA[0] * u + nB[0] * v + nC[0] * w,
+                      nA[1] * u + nB[1] * v + nC[1] * w,
+                      nA[2] * u + nB[2] * v + nC[2] * w]   
+
+    intensity = dot(triangleNormal, render.light_direction) * -1
+    
+    fade = False
+    
+    if intensity > 0:
+        fade = choices([True, False],weights=[(1-intensity)**5,intensity**5])[0]
+    
+    if intensity < 0 or fade:
+        return (0, 0, 0)
+    
+    return (r * intensity, g * intensity, b * intensity)
+
