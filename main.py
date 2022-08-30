1e477a6a2c218992fb4dc0ad0b7cd91667bc5ac0
TheJPMZ <monzon.jpmz@gmail.com>
2022-08-29 21:49:04 -0600

    ♻ Final Refactor

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ window.background = Texture("assets/windows.bmp")
 window.clear_background()
 
 window.active_texture = Texture("assets/pumpkin.bmp")
-window.active_shader = window.shaders["gourad"]
+window.active_shader = window.shaders["negative"]
 window.loadObject("assets/pumpkin.obj",translate=(width-95, height/6, -140), rotate=(-10,190   ,0),scale=(75,75,75))
 
 window.active_texture = Texture("assets/ash.bmp")
-window.active_shader = window.shaders["negative"]
+window.active_shader = window.shaders["gourad"]
 window.loadObject("assets/carro.obj",translate=(width+50, height/12-10, -100), rotate=(-5,240,0),scale=(8, 8, 8))
 
 window.active_texture = Texture("assets/church.bmp")
